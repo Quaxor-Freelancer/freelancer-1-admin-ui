4e8b4532543a0e8b412df4a17eed3da4e850e563
Yathu_NK <yathurshan.18@cse.mrt.ac.lk>
2022-07-12 22:17:03 +0530

    users

--- src/menuItems.js
+++ src/menuItems.js
@@ -1,11 +1,16 @@
 import DashboardIcon from '@mui/icons-material/Dashboard';
-
+import GroupIcon from '@mui/icons-material/Group';
 
 const MenuItems = [
     {
         title: 'Dashboard',
         to: 'dashboard',
         icon: <DashboardIcon />
+    },
+    {
+        title: 'Users',
+        to: 'users',
+        icon: <GroupIcon />
     }
 ]
 
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,9 @@ import AuthenticationLayout from "./components/layout/AuthenticationLayout";
 //pages
 import SignIn from "./pages/authentication/SignIn";
 import SignUp from "./pages/authentication/SignUp";
+import Users from "./pages/users/Users";
+import User from "./pages/User/User";
+import EditUser from "./pages/User/EditUser";
 
 const Routes = () => {
     let router = useRoutes([
@@ -18,6 +21,18 @@ const Routes = () => {
                 {
                     path: "dashboard",
                     element:<>Dashboard</>,
+                },
+                {
+                    path: "users",
+                    element:<Users />,
+                },
+                {
+                    path: "users/:userId",
+                    element:<User />,
+                },
+                {
+                    path: "users/edit/:userId",
+                    element:<EditUser />,
                 },
             ],
         },
