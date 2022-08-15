8d056d23a57008b8e46371d44e63e231736017cd
jegatheesprana <jegatheesprana@gmail.com>
2022-08-15 19:19:56 +0530

    react version fixed

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider, useAuth } from './context/AuthContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render((
   <React.StrictMode>
     <BrowserRouter>
-			<AuthProvider>
-      <App />
+      <AuthProvider>
+        <App />
       </AuthProvider>
-      </BrowserRouter>
+    </BrowserRouter>
 
   </React.StrictMode>
-);
+), document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
